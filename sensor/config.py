ded48cf1a25c37c8c85314d1dcc9cab2bdd0bc92
karthiksamala03 <karthiksamala03@gmail.com>
2022-11-28 01:34:48 +0530

    created congif.py file to get env variables from .env file

---
+++ sensor/config.py
@@ -0,0 +1,14 @@
+
+from dataclasses import dataclass
+import os, sys
+import pymongo
+
+@dataclass()
+class EnvironmentVaraible:
+    mongo_db_url:str = os.getenv("MONGO_DB_URL")
+    aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_access_secret_key;str = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+env_var = EnvironmentVaraible()
+mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
+
