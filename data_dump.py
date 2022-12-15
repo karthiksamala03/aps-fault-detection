ca27443103a738a5d66cceabbab17b2d77e6278e
karthiksamala03 <karthiksamala03@gmail.com>
2022-12-16 01:58:57 +0530

    Production Depolyment Configured

--- data_dump.py
+++ data_dump.py
@@ -2,8 +2,12 @@ import pymongo
 import pandas as pd
 import json
 
+
 # Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+#client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+
+from sensor.config import mongo_client
+client = mongo_client
 
 DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME="aps"
